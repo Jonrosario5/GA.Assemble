18563741ec952da161cb5d5d17c3a6501d11d47a
BryantBuilt <bryantp24@gmail.com>
2019-03-22 13:34:22 -0700

    merging into master

--- static/app.js
+++ static/app.js
@@ -1,24 +1,10 @@
 $(document).ready(function() {
     console.log('jQuery connected')
 
-    let selected_topic = '';
-
-    function updateTopic(newTopic) {
-        selected_topic = newTopic;
-    }
-    
     $(".navbar-burger").click(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
-
-    // $('div .topic').click(function(){
-    //     console.log($(this).attr('data-name'));
-    //     updateTopic($(this).attr('data-name'));
-    //     $('.event-header').text(`Upcoming ${selected_topic} Events`);
-    //     let data = {"selected_topic": selected_topic}
-    //     $.post('/main', JSON.stringify(data));
-    // });
     
     $(`div .event`).click(function(){
         console.log($(this).children('.modal'))
